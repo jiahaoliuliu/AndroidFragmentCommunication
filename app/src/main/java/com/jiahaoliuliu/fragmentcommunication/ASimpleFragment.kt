bece52aae1c4243351f578b681a9384170e70d96
Jiahao Liu <jiahao.liu@talabat.com>
2022-07-15 13:32:50 +0400

    Added the fragment setting result to send the data back to the activity

--- app/src/main/java/com/jiahaoliuliu/fragmentcommunication/ASimpleFragment.kt
+++ app/src/main/java/com/jiahaoliuliu/fragmentcommunication/ASimpleFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 /**
  * A simple [Fragment] subclass.
@@ -12,9 +13,10 @@ import android.view.ViewGroup
  * create an instance of this fragment.
  */
 class ASimpleFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    companion object {
+        const val REQUEST_KEY = "RequestKey"
+        const val CONTENT_KEY = "Content"
+        const val CONTENT_VALUE = "Surprise!"
     }
 
     override fun onCreateView(
@@ -22,6 +24,18 @@ class ASimpleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a_simple, container, false)
+        val view = inflater.inflate(R.layout.fragment_a_simple, container, false)
+        val sendDataBackButton = view.findViewById<Button>(R.id.send_data_back_button)
+        sendDataBackButton.setOnClickListener {
+            sendDataBack()
+        }
+        return view
+    }
+
+    private fun sendDataBack() {
+        val data = Bundle()
+        data.putString(CONTENT_KEY, CONTENT_VALUE)
+        parentFragmentManager.setFragmentResult(REQUEST_KEY, data)
+        activity?.supportFragmentManager?.beginTransaction()?.remove(this)?.commit()
     }
 }
--- app/src/main/java/com/jiahaoliuliu/fragmentcommunication/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/fragmentcommunication/MainActivity.kt
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val dataFromFragmentTextView = findViewById<TextView>(R.id.data_from_fragment_tv)
+        supportFragmentManager.setFragmentResultListener(ASimpleFragment.REQUEST_KEY, this) {
+            _, bundle ->
+            val result = bundle.getString(ASimpleFragment.CONTENT_KEY)
+            dataFromFragmentTextView.text = result
+        }
     }
 }
