042320990a56b18923555f3608da5463d44e993c
Jiahao Liu <jiahao.liu@talabat.com>
2022-07-15 13:21:16 +0400

    Create a simple fragment and attach it to the activity

--- app/src/main/java/com/jiahaoliuliu/fragmentcommunication/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/fragmentcommunication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.jiahaoliuliu.fragmentcommunication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
